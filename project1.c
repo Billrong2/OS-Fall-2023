04192164c2935426a15ad14689e4f9952cb7a2c7
Billrong2 <52747290+Billrong2@users.noreply.github.com>
2023-09-14 14:07:11 -0500

    Update project1.c
    Fixed parallel command
    Awaits fix for redirection

--- project1.c
+++ project1.c
@@ -2,20 +2,19 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 #include <fcntl.h>
-
-
 int main()
 {
-    const char sentencebreaker[] = "&";
+    const char sentencebreaker[] = "&&";
     const char wordbreaker[] = " ";
     size_t length = 0;
-    
     char *input=malloc(sizeof(char)*length);
     char *sentence = NULL;
     char *words = NULL;
     size_t read = 0;
-    char *myargs[9];
+    char *myargs[10];
     myargs[0] = input;
     myargs[8] = NULL;
     while (1)
@@ -28,16 +27,20 @@ int main()
             return 1;
         }
         int i = 0;
-        int j =0;
-        for (int x = 0; input[x] != '\0'; x++) {
+        
+	    int x;
+        for (x = 0; input[x] != '\0'; x++) {
         if (input[x] == '\n') {
             // Shift characters to the left to overwrite the '\r' character
             memmove(&input[x], &input[x + 1], strlen(input) - x);
+            }
         }
-    }
         char *sentence_token = strtok_r(input, sentencebreaker, &sentence);
         while (sentence_token != NULL)
         {
+            
+        //printf("%s \n", sentence_token);
+        //printf(" ---------separate here---------- sentence token above \n");    
             i++;
             char *inner_token = strtok_r(sentence_token, wordbreaker, &words);
             if (inner_token != NULL && i == 1 && (strcmp(inner_token, "BYE") == 0 || strcmp(inner_token, "BYE\n") == 0))
@@ -45,28 +48,50 @@ int main()
                 printf("Goodbye, exiting shell ... \n");
                 exit(EXIT_SUCCESS);
             }
-            myargs[0] = inner_token;
-            //printf("%s", inner_token);
+            int j = 0;
+            myargs[j] = inner_token;
             while (inner_token != NULL)
             {   
                 inner_token = strtok_r(NULL, wordbreaker, &words);
                 j++;
                 myargs[j] = inner_token;
             }
+            printf("%s  %s  %s  %s  %s\n", myargs[0], myargs[1], myargs[2], myargs[3], myargs[4]);
+            printf(" ---------separate here---------- arg above\n");
+            int rc = fork();
+            if (rc == 0){
+                execvp(myargs[0],myargs);
+                printf("Error: Commond Execution Failed . . . \n");
+                printf("returning to shell . . .\n");
+            }
+            else{
+                wait(NULL);
+            }
+            memset(myargs, '\0', sizeof(myargs));
             sentence_token = strtok_r(NULL, sentencebreaker,
                                       &sentence);
-            }
-        int rc = fork();
-        if (rc == 0)
-        { // child (new process)
-            printf("sentence count is %d, word count is %d: \n", i, j);
-            execvp(myargs[0], myargs);
-            printf(" Error: 2cwshould not be here \n");
+
         }
-        else
-        { // parent goes down this path (main)
-            int wc = wait(NULL);
-        }
+
+        //for (x = 0; x <i; i++)
+
+
+
+
+        // int rc = fork();
+        // if (rc == 0)
+        // { // child (new process)
+        //     printf("sentence count is %d, word count is %d: \n", i, j);
+        //     execvp(myargs[0], myargs);
+        //     printf(" Error: 2cwshould not be here \n");
+        // }
+        // else
+        // { // parent goes down this path (main)
+            
+        //     int wc = wait(NULL);
+        //     printf("%d \n", wc);
+        //      printf(" ---------separate here---------- paraent PID \n");
+        // }
     }
     exit(EXIT_SUCCESS);
 }
