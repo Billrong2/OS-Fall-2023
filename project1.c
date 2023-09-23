f0fc55f928bf80768fc5e05f04c489f8264cde5a
Billrong2 <52747290+Billrong2@users.noreply.github.com>
2023-09-22 21:05:55 -0500

    Update project1.c

--- project1.c
+++ project1.c
@@ -5,154 +5,262 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <fcntl.h>
+#include <time.h>
 #define pass (void)0
-char* fetch_program_path(const char *arg1, const char *arg2){
-    int comb_length = snprintf(NULL, 0, "%s%s", arg1, arg2);
+int NUM_OF_PROGRAM_PATH = 0;
+char **programpath = NULL;
+void add_path_root(const char *path){
+    char *newPath = strdup(path);
+    programpath = (char **)realloc(programpath, (NUM_OF_PROGRAM_PATH + 1) * sizeof(char *));
+    programpath[NUM_OF_PROGRAM_PATH] = newPath;
+    NUM_OF_PROGRAM_PATH++;
+}
+void reset_path(){
+    NUM_OF_PROGRAM_PATH = 1;
+    free(programpath);
+    programpath = NULL;
+    programpath = (char **)realloc(programpath, (NUM_OF_PROGRAM_PATH + 1) * sizeof(char *));
+    programpath[0] = "/bin";
+    
+}
+void readfromfile(const char *command)
+{
+    int status = system(command);
+    if (status == -1)
+    {
+        perror("Error executing command");
+    }
+    else
+    {
+        printf("Command executed with status %d\n", status);
+    }
+}
+void printdot()
+{
+    printf(". ");
+    printf(". ");
+    printf(". \n");
+}
+char *fetch_program_path(char *arg1, const char *arg2)
+{
+    int comb_length = snprintf(NULL, 0, "%s %s", arg1, arg2);
     char *combined = (char *)malloc(comb_length + 1);
-    if (combined == NULL) {
+    if (combined == NULL)
+    {
         fprintf(stderr, "Memory allocation failed.\n");
         exit(1);
     }
     snprintf(combined, comb_length + 1, "%s%s", arg1, arg2);
     return combined;
 }
-int main()
+int main(int argc, char *argv[])
 {
-    const char sentencebreaker[] = "&&";
-    const char wordbreaker[] = " ";
-    size_t length = 0;
-    char *input=malloc(sizeof(char)*length);
-    char *sentence = NULL;
-    char *words = NULL;
-    size_t read = 0;
-    char *myargs[10];
-    myargs[0] = input;
-    char *programpath = "/bin";
-    char *redirection[20];
-    redirection[0] = input;
-    myargs[10] = NULL;
-
-    while (1)
+    if (argc == 1)
     {
-        printf("xxr_dash$: ");
-        read = getline(&input, &length, stdin);
-        if (read == -1)
+        // No command-line arguments, run in interactive mode
+        printf("Entering Interactive Mode:\n");
+        printdot();
+        const char sentencebreaker[] = "&";
+        const char wordbreaker[] = " ";
+        size_t length = 100;
+        char *input = malloc(sizeof(char) * length);
+        if (input == NULL)
         {
-            printf("Error reading input.\n");
+            perror("Memory allocation error");
             return 1;
         }
-        int i = 0;
-        
-	    int x;
-        for (x = 0; input[x] != '\0'; x++) {
-        if (input[x] == '\n') {
-            // Shift characters to the left to overwrite the '\r' character
-            memmove(&input[x], &input[x + 1], strlen(input) - x);
-            }
-        }
-        char *sentence_token = strtok_r(input, sentencebreaker, &sentence);
-        while (sentence_token != NULL)
+        char *sentence = NULL;
+        char *defaultpath = ("/bin");
+        char *words = NULL;
+        size_t read = 0;
+        char *myargs[10];
+        myargs[0] = input;
+        add_path_root(defaultpath);
+        char *redirection[20];
+        redirection[0] = input;
+        myargs[10] = NULL;
+        while (1)
         {
-            
-        //printf("%s \n", sentence_token);
-        //printf(" ---------separate here---------- sentence token above \n");    
-            i++;
-            char *inner_token = strtok_r(sentence_token, wordbreaker, &words);
-            if (inner_token != NULL && i == 1 && (strcmp(inner_token, "BYE") == 0 || strcmp(inner_token, "BYE\n") == 0))
-            {
-                printf("Goodbye, exiting shell ... \n");
-                exit(EXIT_SUCCESS);
-            }
+            int i = 0;
             int j = 0;
-            //snprintf(combined, sizeof(combined), "%s%s", programpath, inner_token);
-            
-            myargs[0] = strdup(fetch_program_path(programpath, inner_token));
-
-            while (inner_token != NULL)
-            {   
-                inner_token = strtok_r(NULL, wordbreaker, &words);
-                j++;
-                myargs[j] = inner_token;
+            printf("dash>: ");
+            read = getline(&input, &length, stdin);
+            if (read == -1)
+            {
+                printf("Error reading input.\n");
+                return 1;
             }
-            printf("%s  %s  %s  %s  %s\n", myargs[0], myargs[1], myargs[2], myargs[3], myargs[4]);
-            printf(" ---------separate here---------- arg above\n");
-            int rc = fork();
-            if (rc == 0){
-                int logic_imp;
-                int redirect_counter = 0;
-                int doublecheck =0;
-                for(logic_imp = 0; logic_imp <j;logic_imp++)
+            int x;
+            for (x = 0; input[x] != '\0'; x++)
+            {
+                if (input[x] == '\n')
                 {
-                    if(strcmp(myargs[logic_imp], ">")==0){
-                        for(x = logic_imp; x <= j; x ++){
-                            redirection[redirect_counter] = myargs[x+1];
-                            myargs[logic_imp] = NULL;
-                            redirect_counter++;
-                        }
-                        doublecheck ++;
-                    }
-                    if(doublecheck >1){
-                        printf("Invalid Input, returning shell . . . \n");
+                    memmove(&input[x], &input[x + 1], strlen(input) - x);
+                }
+            }
+            char *sentence_token = strtok_r(input, sentencebreaker, &sentence);
+            while (sentence_token != NULL)
+            {
+                i++;
+                char *inner_token = strtok_r(sentence_token, wordbreaker, &words);
+                myargs[j] = inner_token;
+                while (inner_token != NULL)
+                {
+                    inner_token = strtok_r(NULL, wordbreaker, &words);
+                    j++;
+                    myargs[j] = inner_token;
+                }
+                if (myargs[0] != NULL && (strcmp(myargs[0], "BYE") == 0 || strcmp(myargs[0], "BYE\n") == 0))
+                {
+                    printf("Goodbye, exiting shell ... \n");
+                    exit(EXIT_SUCCESS);
+                }
+                else if (myargs[0] != NULL && (strcmp(myargs[0], "cd") == 0 || strcmp(myargs[0], "cd\n") == 0))
+                {
+                    if (j == 2)
+                    {
+                        printf("Change directory to: %s \n", myargs[1]);
+                        chdir(myargs[1]);
                         break;
                     }
-                    else{
-                        pass;
+                    else
+                    {
+                        printf("Invalid directory path, returning to shell . . . \n");
+                        break;
                     }
                 }
-                int fd;
-                if(doublecheck ==1){
-                    fd = open(*redirection, O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR | S_IRGRP | S_IWGRP | S_IWUSR);
-                    if(fd == -1 && doublecheck ==1){
-                        printf("File Open Failed . . . \n");
-                        printf("returning to shell . . .\n");
+                else if (myargs[0] != NULL && (strcmp(myargs[0], "path") == 0 || strcmp(myargs[0], "path\n") == 0))
+                {
+                    if (j == 1)
+                    {
+                        printf("Default Directory enabled \n");
+                        reset_path();
+                        break;
                     }
-                    dup2(fd, 1);
+                    else
+                    {
+                        int path_num = 1;
+                        while (1)
+                        {
+                            if(myargs[path_num]!=NULL){
+                            add_path_root(myargs[path_num]);
+                            if(programpath[path_num]==NULL)
+                            {
+                                printf("Path Add Failed \n");
+                                break;
+                            }
+                            printf("Path %s Added \n", programpath[path_num]);
+                            printf("%d \n",path_num);
+                            path_num++;
+                            }
+                            else break;
+                        }
+                    }
                 }
-                printf("%s \n", *redirection);
-                printf(" ---------separate here---------- redir above\n");
-                printf("%d \n", doublecheck);
-                printf(" ---------separate here---------- doublecheck above\n");
-                execv(myargs[0],myargs);
-                if(execv(myargs[0], myargs)==-1){
-                    printf("/bin directory not accessable, trying alternative path /usr/bin . . . \n");
-                    programpath = strdup("/usr/bin/");
-                    sleep(0.5);
-                    printf("Type your command again . . . \n");
-                    sleep(0.5);
-                    // char *alt_programpath = "/usr/bin/";
-                    // char *anothertoken = strdup(myargs[1]);
-                    // myargs[0] = strdup(fetch_program_path(alt_programpath, anothertoken));
-                    // execv(myargs[0],myargs);
+                else
+                    pass;
+                char *temp = NULL;
+                temp = myargs[0];
+                myargs[0] = strdup(fetch_program_path(programpath[0], myargs[0]));
+                printf("%s  %s  %s  %s  %s\n", myargs[0], myargs[1], myargs[2], myargs[3], myargs[4]);
+                printf(" ---------separate here---------- arg above\n");
+    for (x = 0; i < NUM_OF_PROGRAM_PATH; i++) {
+        printf("Program %d path: %s\n", i + 1, programpath[i]);
+    }
+                pid_t childpid = fork();
+                if (childpid == 0)
+                {
+                    int logic_imp;
+                    int redirect_counter = 0;
+                    int doublecheck = 0;
+                    for (logic_imp = 0; logic_imp < j; logic_imp++)
+                    {
+                        if (strcmp(myargs[logic_imp], ">") == 0)
+                        {
+                            for (x = logic_imp; x <= j; x++)
+                            {
+                                redirection[redirect_counter] = myargs[x + 1];
+                                myargs[logic_imp] = NULL;
+                                redirect_counter++;
+                            }
+                            doublecheck++;
+                        }
+                        if (doublecheck > 1)
+                        {
+                            printf("Invalid Input, returning shell . . . \n");
+                            break;
+                        }
+                        else
+                        {
+                            pass;
+                        }
+                    }
+                    int fd;
+                    if (doublecheck == 1)
+                    {
+                        fd = open(*redirection, O_WRONLY | O_TRUNC | O_CREAT, S_IRUSR | S_IRGRP | S_IWGRP | S_IWUSR);
+                        if (fd == -1 && doublecheck == 1)
+                        {
+                            printf("File Open Failed . . . \n");
+                            printf("returning to shell . . .\n");
+                        }
+                        dup2(fd, 1);
+                    }
+                    execv(myargs[0], myargs);
+                    int path_counter = 1; // start from 1 because default is 0
+                    printf("original path /bin/ not aviliable \n");
+                    while (1)
+                    {
+                        printf("Path Directory '%s' not accessable, trying alternative path %s . . . \n", programpath[path_counter - 1], programpath[path_counter]);
+                        myargs[0] = strdup(fetch_program_path(programpath[path_counter], temp));
+                        execv(myargs[0], myargs);
+                        //    /bin/clear
+                        path_counter++;
+                        if (programpath[path_counter] == NULL)
+                        {
+                            printf("All PATH Unaviliable, returning to shell \n");
+                            exit(0);
+                        }
+                    }
                 }
+                else{
+                int status;
+                waitpid(childpid, &status, 0);
+                                          }
+                memset(myargs, '\0', sizeof(myargs));
+                sentence_token = strtok_r(NULL, sentencebreaker,
+                                          &sentence);
             }
-            else{
-                wait(NULL);
-            }
-            memset(myargs, '\0', sizeof(myargs));
-            sentence_token = strtok_r(NULL, sentencebreaker,
-                                      &sentence);
-
         }
+    }
 
-        //for (x = 0; x <i; i++)
-
-
-        //free(myargs);
-
-        // int rc = fork();
-        // if (rc == 0)
-        // { // child (new process)
-        //     printf("sentence count is %d, word count is %d: \n", i, j);
-        //     execvp(myargs[0], myargs);
-        //     printf(" Error: 2cwshould not be here \n");
-        // }
-        // else
-        // { // parent goes down this path (main)
-            
-        //     int wc = wait(NULL);
-        //     printf("%d \n", wc);
-        //      printf(" ---------separate here---------- paraent PID \n");
-        // }
+    else if (argc == 2)
+    {
+        printf("Entering Batch Mode \n");
+        printdot();
+        FILE *batchFile = fopen(argv[1], "r");
+        if (batchFile == NULL)
+        {
+            perror("Error opening batch file");
+            return 1;
+        }
+        char command[30];
+        while (fgets(command, sizeof(command), batchFile) != NULL)
+        {
+            size_t len = strlen(command);
+            if (len > 0 && command[len - 1] == '\n')
+            {
+                command[len - 1] = '\0';
+            }
+            printf("Executing: %s\n", command);
+            readfromfile(command);
+        }
+    }
+    else
+    {
+        printf("initilization Failed . . .\n");
+        printf("Terminating\n");
+        exit(0);
     }
     exit(EXIT_SUCCESS);
 }
